9821c1dcd4d4aefe96bb31eb8dd8c4dfecd0df23
Mostakin-MF <mdmostakinali10@gmail.com>
2026-02-15 13:15:36 +0600

    feat: chnage in ProductSection and an animated button demo page.

--- components/tmv-bd/ProductSection.tsx
+++ components/tmv-bd/ProductSection.tsx
@@ -16,7 +16,7 @@ export default function ProductSection() {
                     </p>
                 </div>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center gap-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-center gap-4">
                     <Product />
                     <Product />
                     <Product />
